e73c1ae624a97612f2a83220bd93775498f5e2f3
Haginus <hagiandrei.ah@gmail.com>
2022-02-27 17:04:00 +0200

    Show course cover images

--- src/app/lib/models/course/Course.ts
+++ src/app/lib/models/course/Course.ts
@@ -5,6 +5,7 @@ export interface Course {
   id?: number;
   name: string;
   description: string;
+  coverImage?: string;
   sectionNumber?: number;
   sections?: CourseSection[];
   registration?: CourseRegistration;
